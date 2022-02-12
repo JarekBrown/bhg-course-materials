b4eb0d1241bb8fb3f7c9ec08365cb36f54adf044
Jarek Brown <31749343+JarekBrown@users.noreply.github.com>
2022-02-11 20:40:04 -0700

    additional tests added

--- materials/lab/2/bhg-scanner/scanner/scanner_test.go
+++ materials/lab/2/bhg-scanner/scanner/scanner_test.go
@@ -4,27 +4,71 @@ import (
 	"testing"
 )
 
+func allPorts() ([]int) {
+    var ports []int
+    for i := 1; i <= 1024; i++ {
+        ports = append(ports, i)
+    }
+    return ports
+}
+
 // THESE TESTS ARE LIKELY TO FAIL IF YOU DO NOT CHANGE HOW the worker connects (e.g., you should use DialTimeout)
-func TestOpenPort(t *testing.T){
+func TestOpenPort(t *testing.T) {
+	filename := "out.csv"
+	target := "scanme.nmap.org"
 
-    got := PortScanner() // Currently function returns only number of open ports
-    want := 2 // default value when passing in 1024 TO scanme; also only works because currently PortScanner only returns 
-	          //consider what would happen if you parameterize the portscanner address and ports to scan
+	got, _ := PortScanner(filename, target, allPorts(), false) // Currently function returns only number of open ports
+	want := 2                             // default value when passing in 1024 TO scanme; also only works because currently PortScanner only returns
+	//consider what would happen if you parameterize the portscanner address and ports to scan
 
-    if got != want {
-        t.Errorf("got %d, wanted %d", got, want)
-    }
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
 }
 
-func TestTotalPortsScanned(t *testing.T){
-	// THIS TEST WILL FAIL - YOU MUST MODIFY THE OUTPUT OF PortScanner()
+func TestTotalPortsScanned(t *testing.T) {
+	filename := "out.csv"
+	target := "scanme.nmap.org"
 
-    got := PortScanner() // Currently function returns only number of open ports
-    want := 1024 // default value; consider what would happen if you parameterize the portscanner ports to scan
+	open, closed := PortScanner(filename, target, allPorts(), false) // Currently function returns only number of open ports
+	want := 1024                                // default value; consider what would happen if you parameterize the portscanner ports to scan
 
-    if got != want {
-        t.Errorf("got %d, wanted %d", got, want)
-    }
+	got := open + closed
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
 }
 
+func TestClosedPort(t *testing.T) {
+	filename := "out.csv"
+	target := "scanme.nmap.org"
 
+	_, closed := PortScanner(filename, target, allPorts(), false)
+	want := 1022
+
+	if closed != want {
+		t.Errorf("got %d, wanted %d", closed, want)
+	}
+}
+
+func TestSomePorts(t *testing.T) {
+	filename := "out.csv"
+    target := "scanme.nmap.org"
+
+    ports := make([]int, 2)
+    ports[0] = 80
+    ports[1] = 500
+
+    open,closed := PortScanner(filename, target, ports, false)
+
+    wantOpen := 1
+    if open != wantOpen {
+        t.Errorf("got %d, wanted %d", open, wantOpen)
+    }
+
+    wantClosed := 1
+    if closed != wantClosed {
+        t.Errorf("got %d, wanted %d", closed, wantClosed)
+    }
+}
