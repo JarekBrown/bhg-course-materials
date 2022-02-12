0d573fd6b03f56093f083cb7c4f54a109f383a09
Jarek Brown <31749343+JarekBrown@users.noreply.github.com>
2022-02-11 20:40:16 -0700

    main lab2 files

--- materials/lab/2/bhg-scanner/main/main.go
+++ materials/lab/2/bhg-scanner/main/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "bhg-scanner/scanner"
+import (
+	"bhg-scanner/scanner"
+	"fmt"
+	"time"
+)
 
-func main(){
-	scanner.PortScanner()
+// creates a slice with numbers 1 through 1024
+func allPorts() []int {
+	var ports []int
+	for i := 1; i <= 1024; i++ {
+		ports = append(ports, i)
+	}
+	return ports
+}
+
+func main() {
+	target := "scanme.nmap.org"
+	output := false   //set to true to print
+	file := "out.csv" //file name for output (MUST BE CSV)
+
+	start := time.Now()
+
+	scanner.PortScanner(file, target, allPorts(), output)
+
+	elapsed := time.Since(start)
+	fmt.Printf("Port Scanner took %v\n", elapsed)
 }
--- materials/lab/2/bhg-scanner/scanner/scanner.go
+++ materials/lab/2/bhg-scanner/scanner/scanner.go
@@ -1,27 +1,66 @@
 // bhg-scanner/scanner.go modified from Black Hat Go > CH2 > tcp-scanner-final > main.go
 // Code : https://github.com/blackhat-go/bhg/blob/c27347f6f9019c8911547d6fc912aa1171e6c362/ch-2/tcp-scanner-final/main.go
 // License: {$RepoRoot}/materials/BHG-LICENSE
-// Useage:
-// {TODO 1: FILL IN}
+// Usage:
+// PortScanner uses go channels and DialTimeout from the net package to scan a specified target address/ports
+// Ex.
+// PortScanner("output_file.csv",127.0.0.1, [21,22,80,443], true))
+// ^ this would scan ports 21, 22, 80, and 443 on 127.0.0.1 and output the results to "output_file.csv"
 
 package scanner
 
 import (
 	"fmt"
+	"log"
 	"net"
-	"sort"
+	"os"
+	"time"
 )
 
-//TODO 3 : ADD closed ports; currently code only tracks open ports
-var openports []int  // notice the capitalization here. access limited!
+// Ports contains the results of a port scan
+type Ports struct {
+	open    int            // number of open ports
+	closed  int            // number of closed ports
+	status  map[int]string // stores the status of scanned ports
+	targets []int          // target ports
+}
 
+// newStatus adds the port status to the status map in a Ports object
+// the count of open and closed ports is updated
+func (ports *Ports) newStatus(status bool, num int) {
+	if status == true {
+		ports.open++
+		ports.status[num] = "OPEN"
+	} else {
+		ports.closed++
+		ports.status[num] = "closed"
+	}
+}
 
-func worker(ports, results chan int) {
+// writes port status out to a csv file
+func (ports *Ports) toCSV(filename string) {
+	file, err := os.Create(filename) // create (or overwrite) an output file
+	if err != nil {
+		log.Println("could not create file")
+	}
+	defer file.Close()
+
+	_, err = file.WriteString("port,status\n") // write header for file
+	if err != nil {
+		log.Fatalln("error header to file: ", err)
+	}
+
+	for i := 1; i <= len(ports.status); i++ {
+		file.WriteString(fmt.Sprintf("%v,%v\n", i, ports.status[i])) // write each port status
+	}
+}
+
+func worker(ports, results chan int, target string) {
 	for p := range ports {
-		address := fmt.Sprintf("scanme.nmap.org:%d", p)    
-		conn, err := net.Dial("tcp", address) // TODO 2 : REPLACE THIS WITH DialTimeout (before testing!)
-		if err != nil { 
-			results <- 0
+		address := fmt.Sprintf("%s:%d", target, p)
+		conn, err := net.DialTimeout("tcp", address, 1*time.Second)
+		if err != nil {
+			results <- (p * -1)
 			continue
 		}
 		conn.Close()
@@ -29,43 +68,43 @@ func worker(ports, results chan int) {
 	}
 }
 
-// for Part 5 - consider
-// easy: taking in a variable for the ports to scan (int? slice? ); a target address (string?)?
-// med: easy + return  complex data structure(s?) (maps or slices) containing the ports.
-// hard: restructuring code - consider modification to class/object 
-// No matter what you do, modify scanner_test.go to align; note the single test currently fails
-func PortScanner() int {  
+// scans ports of the specified target address (target ports as well), writing to specified file when output is true
+// returns the number of open ports and the number of closed ports
+func PortScanner(fname, address string, targetPorts []int, output bool) (int, int) {
 
-	ports := make(chan int, 100)   // TODO 4: TUNE THIS FOR CODEANYWHERE / LOCAL MACHINE
+	ports := make(chan int, 100) // TODO 4: TUNE THIS FOR CODEANYWHERE / LOCAL MACHINE
 	results := make(chan int)
 
-	for i := 0; i < cap(ports); i++ {
-		go worker(ports, results)
-	}
+	var scanned Ports
+	scanned.targets = targetPorts
+	scanned.status = make(map[int]string)
 
+	// create workers and scan the target
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, results, address)
+	}
 	go func() {
-		for i := 1; i <= 1024; i++ {
-			ports <- i
+		for i := range scanned.targets {
+			ports <- targetPorts[i]
 		}
 	}()
 
-	for i := 0; i < 1024; i++ {
+	// read the status of each scanned port
+	for i := 0; i < len(scanned.targets); i++ {
 		port := <-results
-		if port != 0 {
-			openports = append(openports, port)
+		if port >= 0 {
+			scanned.newStatus(true, port) // port is open
+		} else {
+			scanned.newStatus(false, (port * -1)) // port is closed
 		}
 	}
-
 	close(ports)
 	close(results)
-	sort.Ints(openports)
 
-	//TODO 5 : Enhance the output for easier consumption, include closed ports
-
-	for _, port := range openports {
-		fmt.Printf("%d open\n", port)
+	// if output desired, print to the csv file
+	if output {
+		scanned.toCSV(fname)
 	}
 
-	return len(openports) // TODO 6 : Return total number of ports scanned (number open, number closed); 
-	//you'll have to modify the function parameter list in the defintion and the values in the scanner_test
+	return scanned.open, scanned.closed
 }
